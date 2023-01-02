78ea3eb100a4d8762a8149d846d47c2b503a5c87
hasindusithmin <hasindusithmin64@gmail.com>
2023-01-02 11:10:21 +0530

    Deploy

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # benchmarksAPI
+
+The BenchmarksAPI is a tool for measuring the performance of web frameworks. Using this API, users can obtain data on the performance of various web frameworks under various workloads, including JSON serialization, single and multiple database queries, rendering a list of fortunes, updating a database record, and rendering a simple plaintext message. These measurements can be useful for developers looking to choose a web framework for their application, as well as for comparing the performance of different frameworks.
+
+[Docs](https://benchmarks.deta.dev/docs)
--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 from fastapi import FastAPI,HTTPException
 import pandas as pd
 from typing import Optional
+from enum import Enum
 
 app = FastAPI(
-    title="benchmarksAPI"
+    title="benchmarksAPI",
+    version="0.2.0",
+    description="The BenchmarksAPI is a tool for measuring the performance of web frameworks. Using this API, users can obtain data on the performance of various web frameworks under various workloads, including JSON serialization, single and multiple database queries, rendering a list of fortunes, updating a database record, and rendering a simple plaintext message. These measurements can be useful for developers looking to choose a web framework for their application, as well as for comparing the performance of different frameworks."
 )
-
 
 @app.get('/')
 async def find_all():
@@ -52,12 +54,23 @@ async def find_one(framework:str,year:Optional[int]=None):
         })
     return frameworks
 
+class Column(Enum):
+    Framework = "framework"
+    Json = "json-serialization"
+    Single = "single-query"
+    Multi = "multiple-query"
+    Fortunes = "fortunes"
+    Updates = "updates"
+    Plaintext = "plaintext"
+    Weighted = "weighted"
+    Year = "year"
+
 @app.get('/sort/{column}')
-async def sort_by_column(column:str,asce:bool=True):
+async def sort_by_column(column:Column,asce:bool=True):
     df = pd.read_csv('frameworks.csv')
-    if column not in list(df.columns.values):
+    if column.value not in list(df.columns.values):
         raise HTTPException(status_code=400,detail="Unrecognized key.")
-    df = df.sort_values(by=column,ascending=asce)
+    df = df.sort_values(by=column.value,ascending=asce)
     frameworks = []
     for id, row in df.iterrows():
         frameworks.append({
@@ -76,12 +89,12 @@ async def sort_by_column(column:str,asce:bool=True):
     return frameworks
 
 @app.get('/{framework}/{column}')
-async def find_one_and_sort(framework:str,column:str,asce:bool=True):
+async def find_one_and_sort(framework:str,column:Column,asce:bool=True):
     df = pd.read_csv('frameworks.csv')
     df = df.query(f"framework == '{framework}'")
-    if column not in list(df.columns.values):
+    if column.value not in list(df.columns.values):
         raise HTTPException(status_code=400,detail="Unrecognized key.")
-    df = df.sort_values(by=column,ascending=asce)
+    df = df.sort_values(by=column.value,ascending=asce)
     frameworks = []
     for id, row in df.iterrows():
         frameworks.append({
